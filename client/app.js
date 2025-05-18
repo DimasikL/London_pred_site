13cbb96af0b204f20b2b90bad834a1836e455e83
Motyapls <matvleontiev@yandex.ru>
2025-05-18 15:06:05 +0300

    add changes

--- client/app.js
+++ client/app.js
@@ -54,6 +54,14 @@ function onClickedEstimatePrice() {
   
 function onPageLoad() {
     console.log( "document loaded" );
+
+    document.getElementById('uiSqft').addEventListener('input', function() {
+    this.value = this.value.replace(/\D/g, '');
+    validateSqft();
+    });
+
+    document.getElementById('uiSqft').addEventListener('blur', validateSqft);
+
     var url = "http://127.0.0.1:5000/get_location_names"; 
     $.get(url,function(data) {
         console.log("got response for get_location_names request");
@@ -68,5 +76,57 @@ function onPageLoad() {
         }
     });
   }
-  
+
+function validateSqft() {
+    const sqftInput = document.getElementById('uiSqft');
+    const errorElement = document.getElementById('sqftError');
+    const value = sqftInput.value;
+
+    if (!/^\d+$/.test(value)) {
+        sqftInput.classList.add('error');
+        errorElement.textContent = "Пожалуйста, введите целое число";
+        return false;
+    }
+    
+    const numValue = parseInt(value);
+    
+    if (numValue < 50 || numValue > 100000) {
+        sqftInput.classList.add('error');
+        errorElement.textContent = "Площадь должна быть от 50 до 100,000 кв.футов";
+        return false;
+    }
+
+    sqftInput.classList.remove('error');
+    errorElement.textContent = "";
+    return true;
+}
+
+function onClickedEstimatePrice() {
+    if (!validateSqft()) {
+        return;
+    }
+    
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bedroom = getBedroomsValue();
+    var bathroom = getBathroomsValue();
+    var reception = getReceptionsValue();
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://127.0.0.1:5000/predict_home_price";
+
+    $.post(url, {
+        sqft: parseFloat(sqft.value),
+        bedroom: bedroom,
+        bathroom: bathroom,
+        reception: reception,
+        location: location.value
+    },function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>£" + data.estimated_price;
+        console.log(status);
+    });
+}
+
 window.onload = onPageLoad;
